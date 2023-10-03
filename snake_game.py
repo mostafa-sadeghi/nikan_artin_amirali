94ebf569827a7b4b4ca2449f98fe6279a017d8db
msa <msai.sadeghi@gmail.com>
2023-10-03 18:47:13 +0330

    s

---
+++ snake_game.py
@@ -0,0 +1,29 @@
+# import turtle
+
+# my_screen = turtle.Screen()
+# my_screen.bgcolor('black')
+# my_screen.setup(600, 600)
+# my_screen.title("Snake")
+
+
+# running = True
+# while running == True:
+#     my_screen.update()
+
+for i in range(5):
+    n = input("Do you want to quit? (yes or no): ")
+    if n == "yes":
+        print("bye")
+        break
+a = True
+while a:
+    n = input("Do you want to quit? (yes or no): ")
+    if n == "yes":
+        print("bye")
+        break
+
+
+# TODO
+"danial,amirali,nikan,artin"
+# با کمک حلقه فور اسم ها را از متن بالا جداد جدا کنید
+# این کار را با حلقه وایل نیز انجام دهید
