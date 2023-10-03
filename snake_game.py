dcf2a817430cce1d277491e6e711b887feeee813
msa <msai.sadeghi@gmail.com>
2023-10-03 18:55:06 +0330

    s

--- snake_game.py
+++ snake_game.py
@@ -10,20 +10,27 @@
 # while running == True:
 #     my_screen.update()
 
-for i in range(5):
-    n = input("Do you want to quit? (yes or no): ")
-    if n == "yes":
-        print("bye")
-        break
-a = True
-while a:
-    n = input("Do you want to quit? (yes or no): ")
-    if n == "yes":
-        print("bye")
-        break
+# for i in range(5):
+#     n = input("Do you want to quit? (yes or no): ")
+#     if n == "yes":
+#         print("bye")
+#         break
+# a = True
+# while a:
+#     n = input("Do you want to quit? (yes or no): ")
+#     if n == "yes":
+#         print("bye")
+#         break
 
 
 # TODO
-"danial,amirali,nikan,artin"
+all_names = "danial,amirali,nikan,artin"
 # با کمک حلقه فور اسم ها را از متن بالا جداد جدا کنید
 # این کار را با حلقه وایل نیز انجام دهید
+# اگر لیست بود که خیلی راحت بود مثل پایین
+my_students = ["danial","amirali","nikan","artin"]
+for student in my_students:
+    print(student)
+
+for i in range(len(my_students)):
+    print(my_students[i])
