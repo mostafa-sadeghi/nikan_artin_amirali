b8f66efbf577309df842d6113f63a1e69deb9845
msa <msai.sadeghi@gmail.com>
2023-11-28 18:59:20 +0330

    s

--- snake_game.py
+++ snake_game.py
@@ -16,13 +16,27 @@ def change_position():
     x = random.randint(-270, 270)
     y = random.randint(-270, 230)
     snake_food.goto(x,y)
-
+def move():
+    if snake_head.direction == "up":
+        ypos = snake_head.ycor()
+        ypos += 20
+        snake_head.sety(ypos)
+        
+def go_up():
+    snake_head.direction = "up"
 
 snake_head = create_turtle("square", "darkgreen")
+snake_head.direction = ""
 snake_food = create_turtle("circle", "red")
 change_position()
+
+screen.listen()
+screen.onkeypress(go_up,"Up")
+
+
 running = True
 while running:
     screen.update()
+    move()
 
 
