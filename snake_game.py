47604c7d7e3773a4f8a137cfb36cde49b9b666d7
msa <msai.sadeghi@gmail.com>
2023-12-05 18:58:15 +0330

    s

--- snake_game.py
+++ snake_game.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+import time
 screen = turtle.Screen()
 screen.bgcolor('black')
 screen.setup(600,600)
@@ -21,22 +22,63 @@ def move():
         ypos = snake_head.ycor()
         ypos += 20
         snake_head.sety(ypos)
-        
+
+    if snake_head.direction == "down":
+        ypos = snake_head.ycor()
+        ypos -= 20
+        snake_head.sety(ypos)
+
+    if snake_head.direction == "right":
+        xpos = snake_head.xcor()
+        xpos += 20
+        snake_head.setx(xpos)
+    if snake_head.direction == "left":
+        xpos = snake_head.xcor()
+        xpos -= 20
+        snake_head.setx(xpos)   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def go_up():
-    snake_head.direction = "up"
+    snake_head.direction = "up" 
+        
+def go_down():
+    snake_head.direction = "down"
+
+def go_right():
+    snake_head.direction = "right"
+
+def go_left():
+    snake_head.direction = "left"
+
+
 
 snake_head = create_turtle("square", "darkgreen")
 snake_head.direction = ""
 snake_food = create_turtle("circle", "red")
 change_position()
-
 screen.listen()
 screen.onkeypress(go_up,"Up")
+screen.onkeypress(go_down,"Down")
+screen.onkeypress(go_right,"Right")
+screen.onkeypress(go_left,"Left")
 
 
 running = True
 while running:
     screen.update()
     move()
+    time.sleep(0.2)
 
 
